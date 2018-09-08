e824920520c30297bc39d68badc81b811b68d30e
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2018-09-08 12:58:22 -0700

    displaying grid search skeletons

--- neuroglancer_annotation_ui/skeletons.py
+++ neuroglancer_annotation_ui/skeletons.py
@@ -40,14 +40,14 @@ def parse_skeleton(path):
 
 
 class SkeletonMeta(object):
-    def __init__(self, name, color=None, scaling=(8, 8, 40)):
+    def __init__(self, name, color=None, scaling=(4, 4, 40)):
         """
 
         :param name: str
             = annotation layer name
         :param color: Hex or RGB
         """
-        self._skeletons = []
+        self._skeletons = {}
         self._name = name
         self._color = color
         self._scaling = np.array(list(scaling))
@@ -64,6 +64,20 @@ class SkeletonMeta(object):
     def color(self):
         return self._color
 
+    @property
+    def skeleton_ids(self):
+        return list(self.skeletons.keys())
+
+    def add_skeletons_from_files(self, paths, skeleton_ids=None):
+        if skeleton_ids is not None:
+            assert len(skeleton_ids) == len(paths)
+        else:
+            skeleton_ids = [None] * len(paths)
+
+        for i_path in range(len(paths)):
+            self.add_skeleton_from_file(paths[i_path], skeleton_ids[i_path])
+
+
     def add_skeleton_from_file(self, path, skeleton_id=None):
         if skeleton_id is None:
             skeleton_id = os.path.basename(path).split(".")[0]
@@ -73,17 +87,26 @@ class SkeletonMeta(object):
 
     def add_skeleton(self, skeleton_id, vertices, edges):
         skeleton = Skeleton(skeleton_id, vertices, edges, scaling=self._scaling)
-        self._skeletons.append(skeleton)
+        self._skeletons[skeleton_id] = skeleton
 
-    def add_to_ngl(self, viewer):
+    def add_to_ngl(self, viewer, skeleton_ids=None):
+        if skeleton_ids is None:
+            skeleton_ids = self.skeleton_ids
+        elif not isinstance(skeleton_ids, list):
+            skeleton_ids = [skeleton_ids]
+
         viewer.add_annotation_layer(self.name, color=self.color)
 
-        for skeleton in self.skeletons:
-            skeleton.add_to_ngl(viewer, self.name, color=self.color)
+        annotations = []
+        for skeleton_id in skeleton_ids:
+            skeleton = self.skeletons[skeleton_id]
+            annotations.extend(skeleton.get_ngl_annotations())
+
+        viewer.add_annotation(self.name, annotations, color=self.color)
 
 
 class Skeleton(object):
-    def __init__(self, skeleton_id, vertices, edges, scaling=(8, 8, 40)):
+    def __init__(self, skeleton_id, vertices, edges, scaling=(4, 4, 40)):
         """
 
         :param skeleton_id: int or str
@@ -94,6 +117,7 @@ class Skeleton(object):
         self._vertices = vertices
         self._edges = edges
         self._scaling = np.array(list(scaling))
+        self._n_branchpoints = None
 
     @property
     def skeleton_id(self):
@@ -124,13 +148,19 @@ class Skeleton(object):
     def scaled_edge_nodes(self):
         return self.scaled_vertices[self.edges]
 
-    def add_to_ngl(self, viewer, layer_name, color=None):
+    @property
+    def n_branchpoints(self):
+        if self._n_branchpoints is None:
+            u_counts = np.unique(self.edges, return_counts=True)[1]
+            self._n_branchpoints = np.sum(u_counts > 2)
+
+        return self._n_branchpoints
+
+    def get_ngl_annotations(self):
         lines = []
         for i_nodes, nodes in enumerate(self.scaled_edge_nodes):
             lines.append(annotation.line_annotation(nodes[0], nodes[1],
                                                     annotation.generate_id(),
                                                     description=str(self.skeleton_id)))
-
-
-        viewer.add_annotation(layer_name, lines, color=color)
+        return lines
 
