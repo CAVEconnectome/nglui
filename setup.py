d769fc25e7f9b15f60dd37389ef7c9a991710856
Casey Schneider-Mizell <caseysm@gmail.com>
2018-10-01 11:41:00 -0700

    Clean-up

--- setup.py
+++ setup.py
@@ -2,19 +2,30 @@ from setuptools import setup, find_packages
 
 setup(name="neuroglancer_annotation_ui", packages=find_packages())
 
-
 setup(
-   name='neuroglancer_analysis',
+   name='neuroglancer_annotation_ui',
    version='0.1',
-   description='Neuroglancer analysis toolkit.',
+   description='Python tools for managing neuroglancer viewers, their states, and python annotations.',
    long_description=open('README.md').read(),
-   author='Derrick Brittain',
-   author_email='dlbrittain@gmail.com',
-   url="https://github.com/dlbrittain/neuroglancer_analysis",
+   author='Casey Schneider-Mizell',
+   author_email='caseysm@gmail.com',
+   url="https://github.com/seung-lab/NeuroglancerAnnotationUI",
    packages=['neuroglancer_annotation_ui'],
    install_requires=[
                 'neuroglancer',
                 'numpy',
+                'pandas',
+                'numpy',
+                'emannotationschemas',
+                'annotationengine',
+                'cloud_volume',
+                'PyChunkedGraph',
+                'grpcio',
+                'pytest',
+                'requests',
+                'marshmallow',
+                'protobuf',
+                'grpc',
                 'tornado==4.5.3',
                 ],  # external packages as dependencies
 )
