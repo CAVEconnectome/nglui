a58120b7018ac6eb53aaac4946a590590c02ac30
Forrest Collman <forrest.collman@gmail.com>
2018-10-23 09:26:54 -0700

    alter setup.py for packages

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import re
 import os
 import codecs
+from setuptools import find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+
 def read(*parts):
     with codecs.open(os.path.join(here, *parts), 'r') as fp:
-    	return fp.read()
+        return fp.read()
+
 
 def find_version(*file_paths):
     version_file = read(*file_paths)
@@ -15,25 +18,27 @@ def find_version(*file_paths):
                               version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError()
+    raise RuntimeError("Unable to find version string.")
 
-with open('requirements.txt','r') as f:
+
+with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
-#with open('test_requirements.txt', 'r') as f:
-#    test_required = f.read().splitlines()
-test_required=[]
+with open('test_requirements.txt', 'r') as f:
+    test_required = f.read().splitlines()
 
 setup(
-    version=find_version("neuroglancer_annotation_ui", "__init__.py"),
-    name='neuroglancer_annotation_ui',
-    description='python tools for interacting with annotations in neuroglancer',
-    author='Casey Schneider-Mizell',
-    author_email='caseys@alleninstitute.org',
-    url='https://github.com/seung-lab/NeuroglancerAnnotationUI',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=required,
-    setup_requires=['pytest-runner'],
-    tests_require=test_required
+   name='neuroglancer_annotation_ui',
+   version='0.1',
+   description='Neuroglancer python annotation UI framework.',
+   long_description=open('README.md').read(),
+   author='Derrick Brittain, Casey Schneider-Mizell, Forrest Collman',
+   author_email='dlbrittain@gmail.com',
+   url="https://github.com/seung-lab/neuroglancer_annotation_ui",
+   packages=find_packages('src'), 
+   package_dir={'': 'src'},
+   setup_requires=['pytest-runner'],
+   tests_require=test_required,
+   include_package_data=True,
+   install_requires=[required],  # external packages as dependencies
 )
