cb89cad9c92a57b98669eaf89656cd3836349a74
sdorkenw <sven.dorkenwald@googlemail.com>
2018-10-25 08:55:40 -0400

    latest

--- neuroglancer_annotation_ui/interface.py
+++ neuroglancer_annotation_ui/interface.py
@@ -4,9 +4,6 @@ import time
 import json
 import urllib
 import copy
-from collections import OrderedDict
-from neuroglancer_annotation_ui import connections
-from neuroglancer_annotation_ui import annotation
 
 
 class Interface(neuroglancer.Viewer):
@@ -25,28 +22,28 @@ class Interface(neuroglancer.Viewer):
         self._state = None
 
     def __repr__(self):
-        return self.viewer.get_viewer_url()
+        return self.get_viewer_url()
 
     @property
     def ngl_url(self):
         return self.ngl_url
 
     def _repr_html_(self):
-        return '<a href="%s" target="_blank">Viewer</a>' % self.viewer.get_viewer_url()
+        return '<a href="%s" target="_blank">Viewer</a>' % self.get_viewer_url()
 
     def add_state(self, state_name):
-        self.states[state_name] = self.viewer.state
+        self.states[state_name] = self.state
         return self.states
 
     def set_state(self, state_name):
-        return self.viewer.set_state(self.states[state_name])
+        return self.set_state(self.states[state_name])
 
     def remove_state(self, state_name):
         del self.states[state_name]
         return self.states
 
     def update_message(self, message):
-        with self.viewer.config_state.txn() as s:
+        with self.config_state.txn() as s:
             if message is not None:
                 s.status_messages['status'] = message
 
@@ -61,8 +58,8 @@ class Interface(neuroglancer.Viewer):
         self.set_state(state)
         self.base_state = copy.deepcopy(self.state)
 
-        self.segment_layer_name = self.viewer.state.layers[1].type
-        return self.viewer
+        self.segment_layer_name = self.state.layers[1].type
+        return self
 
     def add_segmentation_layer(self, layer_name, segmentation_source):
         """Add segmentation layer to viewer instance.
@@ -143,7 +140,7 @@ class Interface(neuroglancer.Viewer):
                 for anno in annotation:
                     s.layers[layer_name].annotations.append(anno)
 
-            # self.current_state = self.viewer.state
+            # self.current_state = self.state
 
         except Exception as e:
             raise e
@@ -156,10 +153,10 @@ class Interface(neuroglancer.Viewer):
             key_command (str): key bindings that provoke action
             method (method): method that is called when key bindings are pressed
         """
-        self.viewer.actions.add(action_name, method)
-        with self.viewer.config_state.txn() as s:
+        self.actions.add(action_name, method)
+        with self.config_state.txn() as s:
             s.input_event_bindings.viewer[key_command] = action_name
-        return self.viewer.config_state
+        return self.config_state
 
     def load_state(self, state_name, state):
         """Load neuroglancer json state to current ngl viewer
@@ -170,14 +167,14 @@ class Interface(neuroglancer.Viewer):
         """
         with open(state, 'r') as f:
             self.state = neuroglancer.decode_json(f.read())
-        self.viewer.set_state(self.state)
+        self.set_state(self.state)
         self.states[state_name] = state
         self._update_state()
-        return self.viewer
+        return self
 
     def save_json(self, file_name):
         try:
-            current_state = self.viewer.state.to_json()
+            current_state = self.state.to_json()
             timestr = time.strftime("%Y%m%d-%H%M%S")
             with open('{}_{}.json'.format(file_name, timestr), 'w') as outfile:
                 json.dump(current_state, outfile)
@@ -189,176 +186,3 @@ class Interface(neuroglancer.Viewer):
 
     def show(self):
         webbrowser.open_new_tab(self.get_viewer_url())
-
-
-class Connector(Interface):
-    def __init__(self):
-        super(Connector, self).__init__()
-        self.data = connections.Connections()
-        self.post_id = None
-        self.pre_id = None
-        self.synapse = []
-        self.index = None
-        self.annotation_layer_name = None
-        self.post_point = None
-        self.pre_point = None
-        self.pre_lines = []
-        self.post_lines = []
-
-    def __repr__(self):
-        return self.viewer.get_viewer_url()
-
-    def _repr_html_(self):
-        return '<a href="%s" target="_blank">Viewer</a>' % self.viewer.get_viewer_url()
-
-    def select_post_process(self, s):
-        if self.post_id is not None:
-            self.update_message("Press 'Shift + C' to clear so a new \
-            postsynaptic process can be assigned.")
-            return
-        self.post_id = self.get_segment(s)
-        if self.post_id == self.pre_id:
-            self.update_message("Cannot assign new postsynaptic process \
-            as current presynaptic process.")
-            return
-        self._update_index()
-        self.post_point = self.add_point(s, 2)
-        self.update_message("Current postsynaptic process \
-         is {}".format(self.post_id))
-        self.add_annotation('Post Synaptic Process', [self.post_point],
-                            '#ff0000')
-
-    def add_point(self, s, description=None):
-        pos = s.mouse_voxel_coordinates
-        if pos is None:
-            return
-        if len(pos) is 3:  # FIXME: bad hack need to revisit
-            id = annotation.generate_id()
-            point = annotation.point_annotation(pos, id, description)
-            return point
-        else:
-            return
-
-    def add_line(self, a, b, description=None):
-        id = annotation.generate_id()
-        line = annotation.line_annotation(a, b, id)
-        return line
-
-    def add_synapse(self, s):
-        if self.post_id is None and self.pre_id is None:
-            self.update_message("Pre and Post targets must be defined before \
-            adding a synapse!!!")
-            return
-        self.synapse = self.add_point(s, 1)
-        self.annotation_layer_name = 'Synapse'
-        pre_line = self.add_line(self.pre_point.point, self.synapse.point)
-        post_line = self.add_line(self.post_point.point, self.synapse.point)
-        self.pre_lines.append(pre_line)
-        self.post_lines.append(post_line)
-        self.add_annotation('Post_connection', self.post_lines, '#ff0000')
-        self.add_annotation('Pre_connection', self.pre_lines, '#00ff24')
-        # append connection to datastruct class...
-        self.data.set_active_pair(self.pre_id, self.post_id)
-        self.data.add_connection(self.pre_point.point.tolist(),
-                                 self.post_point.point.tolist(),
-                                 self.synapse.point.tolist())
-        self.clear_segment(None)
-
-    def select_pre_process(self, s):
-        if self.pre_id is not None:
-            self.update_message("Press 'Shift + V' to clear so a new \
-            presynaptic process can be assigned.")
-            return
-        self.pre_id = self.get_segment(s)
-        if self.pre_id == self.post_id:
-            self.update_message("Cannot assign presynaptic process as current \
-             postsynaptic process.")
-            self.pre_id = None
-            return
-        self._update_index()
-        self.pre_point = self.add_point(s, 0)
-        self.update_message("Current presynaptic is {}".format(self.pre_id))
-        self.add_annotation('Pre Synaptic Process', [self.pre_point], '#00ff24')
-
-    def delete_synapse(self, s):
-        """ TODO
-        Find nearest X,Y,Z point in radius of mouse position and remove index
-        from list import scipy.spatial.KDTree ?? for lookup of xzy pos
-        """
-        self.update_message('Delete key pressed')
-
-    def undo_last_point(self, s):
-        try:
-            with self.viewer.txn() as s:
-                point_layer = s.layers[self.annotation_layer_name]
-                point_layer.annotations = point_layer.annotations[:-1]
-            del self.data.dataset[self.index]['synapses'][-1]
-            self.synapse = self.synapse[:-1]
-            self.update_message("Last Synapse removed!!!")
-        except Exception as e:
-            raise e
-
-    def _update_view(self, pos):
-        with self.viewer.txn() as s:
-            s.voxel_coordinates = pos
-
-    def clear_segment(self, s):
-        self.pre_id = None
-        self.post_id = None
-        self.post_point = None
-        self.pre_point = None
-        self._update_index()
-
-    def _update_index(self):
-        if self.pre_id and self.post_id:
-            self.index = self.data.set_active_pair(self.pre_id, self.post_id)
-        return self.index
-
-    def _clear_annotation_layers(self, s):
-        all_layers = neuroglancer.json_wrappers.to_json(
-            self.viewer.state.layers)
-        new_layers = OrderedDict()
-        for layer in all_layers:
-            if all_layers[layer]['type'] != 'annotation':
-                new_layers[layer] = all_layers[layer]
-
-        with self.viewer.txn() as s:
-            s.layers = neuroglancer.viewer_state.Layers(new_layers)
-
-    def clear_all(self, s):
-        self._clear_annotation_layers(s)
-        self.pre_id = None
-        self.post_id = None
-        self.post_point = None
-        self.pre_point = None
-        self._update_index()
-        self.synapse = []
-
-    def get_segment(self, s):
-        try:
-            return s.selected_values[self.segment_layer_name]
-        except Exception as e:
-            raise e
-
-    def activate_existing_state(self, index):
-        if self.pre_id and self.post_id:
-            return self.set_state_index(index)
-        else:
-            return self.viewer.state
-
-    def set_state_index(self, index):
-        if index in self.states:
-            return self.viewer.set_state(self.states[index])
-        else:
-            return self.viewer.set_state(self.viewer.state)
-
-    # def get_point_list(self, index):
-    #     synapses = []
-    #     synapses = self.states[index].layers['Synapse'].points
-    #     synapses = [np.asarray(l) for l in synapses]
-    #     return synapses
-
-    def save_json(self, s):
-        """ Please delete this """
-        self.data.save_json('example.json')
-        self.update_message("JSON saved")
