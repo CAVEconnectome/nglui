a40c38ac55d2d2b509e1d36a5f5c0e0a7aeafb70
Forrest Collman <forrest.collman@gmail.com>
2018-12-07 16:55:44 -0800

    Bump version: 0.1.0 → 0.1.1

--- src/neuroglancer_annotation_server/nglaunch.py
+++ src/neuroglancer_annotation_server/nglaunch.py
@@ -9,7 +9,7 @@ from annotationframeworkclient.infoservice import InfoServiceClient
 
 mod = Blueprint('nglaunch', 'nglaunch', url_prefix='/annotationui')
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 def setup_manager(info_client, anno_client=None, ngl_url = None):
     manager = AnnotationManager(annotation_client=anno_client)
