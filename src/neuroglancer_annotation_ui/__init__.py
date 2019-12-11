c90c3139827aeb8cca82b31190ca1e408675c132
Forrest Collman <forrest.collman@gmail.com>
2019-12-11 12:17:45 -0800

    Bump version: 0.2.0 → 1.0.0

--- src/neuroglancer_annotation_ui/__init__.py
+++ src/neuroglancer_annotation_ui/__init__.py
@@ -1,3 +1,3 @@
 from neuroglancer_annotation_ui.base import EasyViewer
 from neuroglancer_annotation_ui.utils import set_static_content_source, stop_ngl_server
-__version__ = "0.2.0"
+__version__ = "1.0.0"
