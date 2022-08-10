84e4ea5db5a0a6220fe168f2c8999cc1f9273c15
Forrest Collman <forrest.collman@gmail.com>
2022-08-09 21:16:55 -0700

    added docs removed stray code

--- src/nglui/statebuilder/helpers.py
+++ src/nglui/statebuilder/helpers.py
@@ -5,6 +5,7 @@ from .layers import (
     PointMapper,
 )
 from .statebuilder import ChainedStateBuilder, StateBuilder
+from caveclient import CAVEclient
 
 CONTRAST_CONFIG = {
     "minnie65_phase3_v1": {
@@ -16,8 +17,22 @@ CONTRAST_CONFIG = {
 
 
 def make_point_statebuilder(
-    client, point_column="pt_position", linked_seg_column="pt_root_id", contrast=None
+    client: CAVEclient,
+    point_column="pt_position",
+    linked_seg_column="pt_root_id",
+    contrast=None,
 ):
+    """make a state builder that puts points on a single column with a linked segmentaton id
+
+    Args:
+        client (CAVEclient): CAVEclient configured for the datastack desired
+        point_column (str, optional): column in dataframe to pull points from. Defaults to "pt_position".
+        linked_seg_column (str, optional): column to link to segmentation, None for no column. Defaults to "pt_root_id".
+        contrast (_type_, optional): _description_. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
     img_layer, seg_layer = from_client(client, contrast=contrast)
     point_mapper = PointMapper(
         point_column=point_column, linked_segmentation_column=linked_seg_column
@@ -31,40 +46,47 @@ def make_point_statebuilder(
     )
 
 
-seg_layer = SegmentationLayerConfig(seg_source, selected_ids_column="post_pt_root_id")
-
-postsyn_mapper = LineMapper(
-    point_column_a="pre_pt_position", point_column_b="ctr_pt_position"
-)
-postsyn_annos = AnnotationLayerConfig(
-    "post", color="#00CCCC", mapping_rules=postsyn_mapper
-)
-
-postsyn_sb = StateBuilder(layers=[img_layer, seg_layer, postsyn_annos])
-
-# Second state builder
-presyn_mapper = LineMapper(
-    point_column_a="ctr_pt_position", point_column_b="post_pt_position"
-)
-presyn_annos = AnnotationLayerConfig(
-    "pre", color="#CC1111", mapping_rules=presyn_mapper
-)
-
-presyn_sb = StateBuilder(layers=[presyn_annos])
-
-
 def make_pre_post_statebuilder(
-    client,
-    root_id,
-    show_inputs=False,
-    show_outputs=False,
-    contrast=None,
+    client: CAVEclient,
+    root_id: int,
+    show_inputs: bool = False,
+    show_outputs: bool = False,
+    contrast: dict = None,
+    view_kws: dict = None,
     point_column="ctr_pt_position",
-    view_kws=None,
+    pre_pt_root_id_col="pre_pt_root_id",
+    post_pt_root_id_col="post_pt_root_id",
+    input_layer_name="syns_in",
+    output_layer_name="syns_out",
 ):
+    """Function to generate ChainedStateBuilder with optional pre and post synaptic
+    annotation layers
+
+    Args:
+        client (CAVEclient): a CAVEclient configured for datastack to visualize
+        root_id (_type_): a rootID to build around
+        show_inputs (bool, optional): whether to show input synapses. Defaults to False.
+        show_outputs (bool, optional): whether to show output synapses.. Defaults to False.
+        contrast (list, optional):  list-like, optional
+            Two elements specifying the black level and white level as
+            floats between 0 and 1, by default None. If None, no contrast
+            is set.
+        view_kws (_type_, optional): _description_. Defaults to None.
+        point_column (str, optional): _description_. Defaults to "ctr_pt_position".
+        pre_pt_root_id_col (str, optional): _description_. Defaults to "pre_pt_root_id".
+        post_pt_root_id_col (str, optional): _description_. Defaults to "post_pt_root_id".
+        input_layer_name (str, optional): _description_. Defaults to "syns_in".
+        output_layer_name (str, optional): _description_. Defaults to "syns_out".
+
+    Returns:
+        ChainedStateBuilder: An instance of a ChainedStateBuilder configured to accept
+        a list  starting with None followed by optionally synapse input dataframe
+        followed by optionally synapse output dataframe.
+    """
 
     img_layer, seg_layer = from_client(client, contrast=contrast)
     seg_layer.add_selection_map(fixed_ids=root_id)
+    seg_layer.color
     sb1 = StateBuilder(
         layers=[img_layer, seg_layer],
         resolution=client.info.viewer_resolution(),
@@ -76,19 +98,19 @@ def make_pre_post_statebuilder(
         # First state builder
 
         input_point_mapper = PointMapper(
-            point_column=point_column, linked_segmentation_column="pre_pt_root_id"
+            point_column=point_column, linked_segmentation_column=pre_pt_root_id_col
         )
         inputs_lay = AnnotationLayerConfig(
-            "syns_in", mapping_rules=[input_point_mapper]
+            input_layer_name, mapping_rules=[input_point_mapper]
         )
         sb_in = StateBuilder([inputs_lay])
         state_builders.append(sb_in)
     if show_outputs:
         output_point_mapper = PointMapper(
-            point_column=point_column, linked_segmentation_column="post_pt_root_id"
+            point_column=point_column, linked_segmentation_column=post_pt_root_id_col
         )
         outputs_lay = AnnotationLayerConfig(
-            "syns_in", mapping_rules=[output_point_mapper]
+            output_layer_name, mapping_rules=[output_point_mapper]
         )
         sb_out = StateBuilder([outputs_lay])
         state_builders.append(sb_out)
@@ -102,10 +124,39 @@ def make_neuron_neuroglancer_link(
     show_outputs=False,
     contrast=None,
     timestamp=None,
-    point_column="ctr_pt_position",
     view_kws=None,
+    point_column="ctr_pt_position",
+    pre_pt_root_id_col="pre_pt_root_id",
+    post_pt_root_id_col="post_pt_root_id",
+    input_layer_name="syns_in",
+    output_layer_name="syns_out",
     ngl_url=None,
 ):
+    """function to create a neuroglancer link view of a neuron, optionally including inputs and outputs
+
+    Args:
+        client (_type_): a CAVEclient configured for datastack to visualize
+        root_id (_type_): root_id to build around
+        show_inputs (bool, optional): whether to include input synapses. Defaults to False.
+        show_outputs (bool, optional): whether to include output synapses. Defaults to False.
+        contrast (list, optional): Two elements specifying the black level and white level as
+            floats between 0 and 1, by default None. If None, no contrast
+            is set.
+        timestamp (datetime.datetime, optional): timestamp to do query. Defaults to None, will use materialized version.
+        view_kws (dict, optional): view_kws to configure statebuilder, see nglui.StateBuilder . Defaults to None.
+        point_column (str, optional): column to pull points for synapses from. Defaults to "ctr_pt_position".
+        pre_pt_root_id_col (str, optional): column to pull pre synaptic ids for synapses from. Defaults to "pre_pt_root_id".
+        post_pt_root_id_col (str, optional): column to pull post synaptic ids for synapses from. Defaults to "post_pt_root_id".
+        input_layer_name (str, optional): name of layer for inputs. Defaults to "syns_in".
+        output_layer_name (str, optional): name of layer for outputs. Defaults to "syns_out".
+        ngl_url (str, optional): url to use for neuroglancer. Defaults to None (will use default viewer set in datastack)
+
+    Raises:
+        ValueError: If the point column is not present in the synapse table
+
+    Returns:
+        str: url of neuroglancer link with saved state
+    """
     dataframes = [None]
     if show_inputs:
         syn_in_df = client.materialize.synapse_query(
@@ -131,10 +182,14 @@ def make_neuron_neuroglancer_link(
         contrast=contrast,
         point_column=point_column,
         view_kws=view_kws,
+        pre_pt_root_id_col=pre_pt_root_id_col,
+        post_pt_root_id_col=post_pt_root_id_col,
+        input_layer_name=input_layer_name,
+        output_layer_name=output_layer_name,
     )
     state_d = sb.render_state(dataframes, return_as="json")
     link_id = client.state.upload_state_json(state_d)
-    url = client.state.build_neuroglancer_url(link_id, ng_url=ng_url)
+    url = client.state.build_neuroglancer_url(link_id, ngl_url=ngl_url)
     return url
 
 
